584959d35c9523579207a0a8cd1784143cdcf247
Miltos Poulizos <mpoulizos@gmail.com>
2021-12-06 13:46:10 +0200

    Initial import: Transfer from other repository

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
-# mpc
-Lerna noise API
-GK: needs to include libxml2 path when compiling, -lcrypto -lssl -pthread -lxml2 when linking, and run with port number.
+# Lerna mpc
+> Lerna noise API
+
+
+## Table of contents
+
+- [Usage](#Usage)
+  - [Compile](#Compile)
+  - [Prerequisites](#prerequisites)
+  - [Running the application](#Run)
+
+## Usage
+
+### Compile
+
+For compiling the mpc server use the make utility.
+
+```bash
+# compile project
+#
+make
+```
+
+Note: needs to include libxml2 path when compiling, -lcrypto -lssl -pthread -lxml2 when linking, and run with port number.
+
+### Prerequisites
+
+To run Lerna mpc we need to provide valid certificate files under the following paths
+
+```bash
+# Certificate file
+./myCA/cacert.pem
+
+# Private key file
+./myCA/private/cakey.pem
+```
+
+### Run
+
+Lerna mpc is a stand alone application, in order to run application use the following command.
+
+```bash
+# Run the server
+#
+./noiseapi
+```
---
+++ main.cpp
@@ -0,0 +1,471 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: gkellaris
+ *
+ * Created on August 24, 2020, 2:01 PM
+ */
+
+//SSL-Server.c
+
+#include <errno.h>
+#include <unistd.h>
+#include <malloc.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <resolv.h>
+#include "openssl/ssl.h"
+#include "openssl/err.h"
+#include <pthread.h>
+#include <unordered_map>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+#include <libxml/xmlstring.h>
+#include <openssl/rand.h>
+#include <mutex>
+#include <list>
+#include <math.h>
+
+
+#define FAIL    -1
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+std::unordered_map<int, std::list<std::pair<double,int>>> shares;
+std::mutex shares_mutex;
+std::unordered_map<unsigned int, std::unordered_map<std::string, std::string>> jobs;
+std::mutex jobs_mutex;
+
+int OpenListener(int port)
+{   int sd;
+    struct sockaddr_in addr;
+
+    sd = socket(PF_INET, SOCK_STREAM, 0);
+    bzero(&addr, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    if ( bind(sd, (struct sockaddr*)&addr, sizeof(addr)) != 0 )
+    {
+        perror("can't bind port");
+        abort();
+    }
+    if ( listen(sd, 50) != 0 )
+    {
+        perror("Can't configure listening port");
+        abort();
+    }
+    return sd;
+}
+
+SSL_CTX* InitServerCTX(void)
+{   const SSL_METHOD *method;
+    SSL_CTX *ctx;
+
+    OpenSSL_add_all_algorithms();  /* load & register all cryptos, etc. */
+    SSL_load_error_strings();   /* load all error messages */
+    method = TLS_server_method();  /* create new server-method instance */
+    ctx = SSL_CTX_new(method);   /* create new context from method */
+    if ( ctx == NULL )
+    {
+        ERR_print_errors_fp(stderr);
+        abort();
+    }
+    return ctx;
+}
+
+void LoadCertificates(SSL_CTX* ctx, char* CertFile, char* KeyFile)
+{
+    //New lines 
+    if (SSL_CTX_load_verify_locations(ctx, CertFile, KeyFile) != 1)
+        ERR_print_errors_fp(stderr);
+
+    if (SSL_CTX_set_default_verify_paths(ctx) != 1)
+        ERR_print_errors_fp(stderr);
+    //End new lines
+
+    /* set the local certificate from CertFile */
+    if (SSL_CTX_use_certificate_file(ctx, CertFile, SSL_FILETYPE_PEM) <= 0)
+    {
+        ERR_print_errors_fp(stderr);
+        abort();
+    }
+    /* set the private key from KeyFile (may be the same as CertFile) */
+    if (SSL_CTX_use_PrivateKey_file(ctx, KeyFile, SSL_FILETYPE_PEM) <= 0)
+    {
+        ERR_print_errors_fp(stderr);
+        abort();
+    }
+    /* verify private key */
+    if (!SSL_CTX_check_private_key(ctx))
+    {
+        fprintf(stderr, "Private key does not match the public certificate\n");
+        abort();
+    }
+
+    //New lines - Force the client-side have a certificate
+    SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);
+    SSL_CTX_set_verify_depth(ctx, 4);
+    //End new lines
+}
+
+bool ShowCerts(SSL* ssl)
+{   X509 *cert;
+    char *line;
+
+    cert = SSL_get_peer_certificate(ssl); /* Get certificates (if available) */
+    if ( cert != NULL )
+    {
+        //printf("Client certificates:\n");
+        line = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0);
+        //printf("Subject: %s\n", line);
+        free(line);
+        line = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0);
+        printf("Issuer: %s\n", line);
+        free(line);
+        X509_free(cert);
+        return true;
+    }
+    else{
+        //printf("No certificates.\n");
+        return false;
+    }
+}
+
+static int
+print_element_names(xmlNode * a_node, double *share, bool co)
+{
+    xmlNode *cur_node = NULL;
+    //double a=0.001;
+    //double a=1;
+
+    
+    
+    for (cur_node = a_node; cur_node; cur_node = cur_node->next) {
+        if (cur_node->type == XML_ELEMENT_NODE) {
+            xmlChar *val = xmlNodeGetContent(cur_node);
+            //printf("node type: Element, name: %s, value: %s\n", cur_node->name, val);
+            
+            if((!xmlStrcmp(cur_node->name, (const xmlChar *)"CompID"))){
+                int id;
+                sscanf((char *)val,"%d",&id);
+                if(co){
+                if(id==0){
+                    unsigned int rnum[1];
+                    
+                    
+                    RAND_bytes((unsigned char *)&rnum[0], sizeof(rnum));
+                    cur_node = cur_node->next;
+                    xmlChar *val1 = xmlNodeGetContent(cur_node);
+                    //printf("Adding node type: Element, name: %s, value: %s\n", cur_node->name, val1);
+                    std::lock_guard<std::mutex> guard(jobs_mutex);
+                    //std::string s1((char *)val1);
+                    jobs[rnum[0]][(char *)cur_node->name]=(char *)val1;
+                    
+                    
+                    cur_node = cur_node->next;
+                    xmlChar *val2 = xmlNodeGetContent(cur_node);
+                   // printf("Adding node type: Element, name: %s, value: %s\n", (char *)cur_node->name, (char *)val2);
+                    
+                    //std::string s2((char *)val2);
+                    jobs[rnum[0]][(char *)cur_node->name]=(char *)val2;
+                    
+                    
+                    cur_node = cur_node->next;
+                    xmlChar *val3 = xmlNodeGetContent(cur_node);
+                   // printf("Adding node type: Element, name: %s, value: %s\n", (char *)cur_node->name, (char *)val2);
+                    
+                    //std::string s2((char *)val2);
+                    jobs[rnum[0]][(char *)cur_node->name]=(char *)val3;
+                    
+                    
+                    cur_node = cur_node->next;
+                    xmlChar *val4 = xmlNodeGetContent(cur_node);
+                   // printf("Adding node type: Element, name: %s, value: %s\n", (char *)cur_node->name, (char *)val2);
+                    
+                    //std::string s2((char *)val2);
+                    jobs[rnum[0]][(char *)cur_node->name]=(char *)val4;
+//                    cur_node = cur_node->next;
+//                    xmlChar *val3 = xmlNodeGetContent(cur_node);
+//                    printf("Adding node type: Element, name: %s, value: %s\n", (char *)cur_node->name, (char *)val3);
+//                    
+//                    //std::string s2((char *)val2);
+//                    jobs[rnum[0]][(char *)cur_node->name]=(char *)val3;
+                    
+                    
+                    //printf("%s\n",jobs[rnum[0]]["DP"].c_str());
+                    xmlFree(val);
+                    xmlFree(val1);
+                    xmlFree(val2);
+                    //xmlFree(val3);
+                    return rnum[0];
+                }else{
+                    
+                    if(jobs.find(id) == jobs.end()){
+                        std::lock_guard<std::mutex> guard2(shares_mutex);
+                        shares.erase(id);
+                        return 0;
+                    }
+                double eps;
+                double sens;
+                double a;
+                int d;
+                sscanf(jobs[id]["DP"].c_str(),"%lf",&eps);
+                
+                
+                
+                sscanf(jobs[id]["Wsize"].c_str(),"%d",&d);
+                sscanf(jobs[id]["a"].c_str(),"%lf",&a);
+                //sscanf(jobs[id]["Sensitivity"].c_str(),"%lf",&sens);
+               
+                    std::lock_guard<std::mutex> guard1(jobs_mutex);
+                    jobs.erase(id);
+                    double sum=0.0;
+                    int min=INT_MAX;
+                    for (std::pair<double,int> n : shares[id]) {
+                        sum+=n.first;
+                        if(min>n.second)
+                            min=n.second;
+                    }
+                    sens=2.0/(a*min);
+                     printf("EPS value: %lf\nSensitivity: %lf\nk: %d\nW_size: %d\nalpha: %lf\n",eps,sens,min,d,a);
+                if(eps>0.00000001){
+                    for(int i=0;i<d;i++){
+                        int rnum[1];
+                        RAND_bytes((unsigned char *)&rnum[0], sizeof(rnum));
+                        double rnd=((double)rnum[0]/(double)(INT_MAX))/2.0;
+                        //printf("Random value between -0.5 and 0.5: %lf",rnd);0;
+                        //
+                        //compute laplace noise
+                        if(rnd<0.0){
+                            share[i]=sens/eps*log(1.0-2.0*abs(rnd));
+                        }else{
+                            share[i]=(-1.0)*sens/eps*log(1.0-2.0*abs(rnd));
+                        }
+                        printf("Noisy value: %lf\n",share[i]);
+                        share[i]*=100000000;
+                    }
+                }
+                    
+                    if(shares[id].size()>1)
+                        for(int i=0;i<d;i++)
+                            share[i]+=(-1.0)*sum;
+                    else
+                        for(int i=0;i<d;i++)
+                            share[i]=0.0;
+                    
+                    std::lock_guard<std::mutex> guard2(shares_mutex);
+                    shares.erase(id);
+                    
+                    return d; //this is a hack because even without erasing id, we cannot read Wsize outside this function for some reason...
+                }
+                }
+                else{
+                    if(jobs.find(id) == jobs.end())
+                        return 0;
+                    int rnum[1];
+                    RAND_bytes((unsigned char *)&rnum[0], sizeof(rnum));
+                    //cur_node = cur_node->next;
+                    //xmlChar *val1 = xmlNodeGetContent(cur_node);
+                    //printf("Adding node type: Element, name: %s, value: %s\n", cur_node->name, val1);
+                    //int sh;
+                    //sscanf((char *)val1,"%ld",&cid);
+                    //std::pair<int,bool> temp;
+                    //temp.first=rnum[0];
+                    //temp.second=false;
+                    
+                    
+                    xmlFree(val);
+                    //xmlFree(val1);
+                    share[0]=rnum[0];
+                    return id;
+                }
+            }
+            
+        }
+
+        //print_element_names(cur_node->children);
+    }
+}
+
+int parsemsg(char* xml, int length, double *share, bool co){
+    xmlDocPtr doc;
+    xmlNode *root_element = NULL;
+    doc = xmlReadMemory(xml, length, "noname.xml", NULL, 0);
+    if (doc == NULL) {
+        fprintf(stderr, "Failed to parse document\n");
+	return -1;
+    }else
+    {
+        root_element = xmlDocGetRootElement(doc);
+        int id=print_element_names(root_element->children, share,co);
+        xmlFreeDoc(doc);
+        return id;
+    }
+    
+}
+
+void* Servlet(void *arg) /* Serve the connection -- threadable */
+{   
+    SSL *ssl=((SSL *)arg);
+    char buf[4096];
+    char reply[4096];
+    int sd, bytes;
+    
+
+    //printf("Thread id: %lu\n", pthread_self());
+    if ( SSL_accept(ssl) == FAIL )     /* do SSL-protocol accept */
+        ERR_print_errors_fp(stderr);
+    else
+    {
+        if(ShowCerts(ssl)){        /* get any certificates */
+            const char* echo;
+        bytes = SSL_read(ssl, buf, sizeof(buf)); /* get request */
+        if ( bytes > 0 ){
+            buf[bytes] = 0;
+            //printf("Coordinator msg: \"%s\"\n", buf);
+            double share[10000];
+            share[0]=-1.0;
+            
+           // printf("Array 0: %lf\n", share[0]);
+            int id=parsemsg(buf, bytes, share, true);
+            char vOut [12];
+            snprintf(vOut, sizeof(vOut), "%u", id);
+            
+            //printf("Array 0: %lf\n", share[0]);
+            
+            if(share[0]==-1.0){
+                echo="<Body><CompID>%s</CompID></Body>";
+                 sprintf(reply, echo, vOut);
+                 SSL_write(ssl, reply, strlen(reply));
+            }else{
+                
+                //add DP if necessary, delete job
+                
+                
+               
+                
+                
+                std::string total="<Body>";
+               // printf("d: %d\n", d);
+                for(int i=0;i<id;i++){
+                    echo="<Share%s>%s</Share%s>";
+                    char vtmp [20];
+                    char vOut2 [20];
+                    snprintf(vtmp, sizeof(vOut2), "%d", i);
+                    snprintf(vOut2, sizeof(vOut2), "%lf", share[i]);
+                    sprintf(reply, echo, vtmp, vOut2,vtmp);
+                    //printf("Sending share %lf to Coordinator\n", share[i]);
+                    total+=reply;
+                    
+                }
+                total+="</Body>";
+                //printf("%d\n",strlen(total.c_str()));
+                SSL_write(ssl, total.c_str(), strlen(total.c_str()));
+            }
+            
+             //sprintf(reply, echo, vOut);
+            //sprintf(reply, echo, buf);   /* construct reply */
+             /* send reply */
+        }
+        else
+            ERR_print_errors_fp(stderr);
+    }else{
+            const char* echo="<Body><CompID>%s</CompID><Share>%s</Share><Epsilon>%s</Epsilon></Body>";
+            bytes = SSL_read(ssl, buf, sizeof(buf)); /* get request */
+        if ( bytes > 0 )
+        {
+            buf[bytes] = 0;
+            //printf("Worker msg: \"%s\"\n", buf);
+            double share[1];
+            share[0]=0.0;
+            int id=parsemsg(buf, bytes, share, false);
+            //int share=shares[id][cid].first;
+            char vOut1 [12];
+            snprintf(vOut1, sizeof(vOut1), "%u", id);
+            char vOut2 [20];
+            snprintf(vOut2, sizeof(vOut2), "%lf", share[0]);
+            printf("Sending share %lf for job %u to Worker\n", share[0], id);
+            sprintf(reply, echo, vOut1,vOut2,jobs[id]["DP"].c_str());   /* construct reply */
+            SSL_write(ssl, reply, strlen(reply)); /* send reply */
+            buf[0]=0;
+            bytes = SSL_read(ssl, buf, sizeof(buf)); /* get response */
+            
+            if ( bytes > 0 ){
+                buf[bytes] = 0;
+                //printf("Worker msg: \"%s\"\n", buf);
+                if(strcmp(buf,"OK")==0&&jobs.find(id) != jobs.end()){
+                    buf[0]=0;
+                    bytes = SSL_read(ssl, buf, sizeof(buf));
+                    buf[bytes] = 0;
+                    int k;
+                    sscanf(buf,"%d",&k);
+                    std::pair<double,int> tmp;
+                    tmp.first=share[0];
+                    tmp.second=k;
+                    std::lock_guard<std::mutex> guard(shares_mutex);
+                    shares[id].push_front(tmp);
+                    //printf("Client Accepted\n");
+                }
+                else
+                    printf("Client Rejected or Timed-out\n");
+            }
+        }
+        else
+            ERR_print_errors_fp(stderr);
+    }
+    }
+    sd = SSL_get_fd(ssl);       /* get socket connection */
+    SSL_free(ssl);         /* release SSL state */
+    close(sd);          /* close connection */
+}
+
+int main()
+{   
+    SSL_CTX *ctx;
+    int server;
+    char portnum[]="31337";
+
+        char CertFile[] = "./myCA/cacert.pem";
+        char KeyFile[] = "./myCA/private/cakey.pem";
+
+    SSL_library_init();
+
+    ctx = InitServerCTX();        /* initialize SSL */
+    LoadCertificates(ctx, CertFile, KeyFile); /* load certs */
+    server = OpenListener(atoi(portnum));    /* create server socket */
+    pthread_t tid[60];
+    int i = 0;
+    printf("Server started\n");
+    for(;;)
+    {   struct sockaddr_in addr;
+        socklen_t len = sizeof(addr);
+        SSL *ssl;
+
+        int client = accept(server, (struct sockaddr*)&addr, &len);  /* accept connection as usual */
+        //printf("Connection: %s:%hu\n",inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+        ssl = SSL_new(ctx);              /* get new SSL state with context */
+        SSL_set_fd(ssl, client);      /* set connection socket to SSL state */
+        if( pthread_create(&tid[i++], NULL, Servlet, (void *) ssl) != 0 )
+           printf("Failed to create thread\n");
+        if( i >= 50)
+        {
+          i = 0;
+          while(i < 50)
+          {
+            pthread_join(tid[i++],NULL);
+          }
+          i = 0;
+        }
+        //Servlet(ssl);         /* service connection */
+    }
+    close(server);          /* close server socket */
+    SSL_CTX_free(ctx);         /* release context */
+}
+
